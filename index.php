5c3d03c1fc23467d92d3d702273af50f38934140
ankiiitt <43487188+ankiiitt@users.noreply.github.com>
2021-04-09 12:50:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
 
-print "WELCOME TO THE OPENSHIFT WORLD. THIS IS SESSION SECOND..!!";
+print "WELCOME TO THE OPENSHIFT WORLD. THIS IS SESSION SECOND..First Code Run Sucessful And this is next time i change the code !!";
 
 ?>
